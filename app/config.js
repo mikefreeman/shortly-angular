8bcb7541774ce7303d7effb96fbc66cfc1a6f787
Hack Reactor Students <michael.p.freeman@gmail.com>
2014-05-16 22:25:57 -0700

    Migrating to Angular

---
+++ app/config.js
@@ -0,0 +1,84 @@
+var mongoose = require('mongoose');
+
+mongoURI = process.env.mongourl || "localhost:27017";
+mongoose.connect(mongoURI);
+
+
+// Run in seperate terminal window using 'mongod'
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+ console.log('Mongodb connection open');
+});
+
+module.exports = db;
+
+// var mongoose = require('mongoose');
+
+
+
+// var urls = new mongoose.Schema({
+//     url: { 'type': String },
+//     base_url: { 'type': String },
+//     code: { 'type': Number },
+//     title: { 'type': String },
+//     visits: { 'type': Number }
+//   });
+
+// var User = mongoose.model('Users', users);
+// var Link = mongoose.model('Links', urls);
+
+
+
+// exports.User = User;
+// exports.Link = Link;
+
+// var user = new User({username : "blah"});
+// user.save();
+
+
+// var Bookshelf = require('bookshelf');
+// var path = require('path');
+
+// var db = Bookshelf.initialize({
+//   client: 'sqlite3',
+//   connection: {
+//     host: '127.0.0.1',
+//     user: 'your_database_user',
+//     password: 'password',
+//     database: 'shortlydb',
+//     charset: 'utf8',
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   }
+// });
+
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('base_url', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// module.exports = db;
---
+++ app/models/user.js
@@ -0,0 +1,29 @@
+var db = require('../config');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true }
+});
+
+userSchema.pre('save', function(next){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+var User = mongoose.model('User', userSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = User;
